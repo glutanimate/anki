79694633a810a9a356fc3aff9d1d2f852e4e8b3b
Glutanimate <glutanimate@users.noreply.github.com>
2023-10-13 17:42:17 +0200

    Make generic Callback parameter optional

--- ts/lib/hooks.ts
+++ ts/lib/hooks.ts
@@ -1,7 +1,7 @@
 // Copyright: Ankitects Pty Ltd and contributors
 // License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-export type Callback<T> = (arg?: T) => void | Promise<void>;
+export type Callback<T = void> = (arg?: T) => void | Promise<void>;
 export type Filter<T> = (arg: T) => T | Promise<T>;
 
 export function runHook<A>(
